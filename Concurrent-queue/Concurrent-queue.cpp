d9380ed9330fb92507e55a2797a54f0202b4b9db
Lucas Dias <ldias.santos.83@gmail.com>
2025-07-07 20:51:46 +0200

    Fix ident

--- Concurrent-queue/Concurrent-queue.cpp
+++ Concurrent-queue/Concurrent-queue.cpp
@@ -3,7 +3,6 @@
 #include <atomic>
 #include <array>
 
-
 struct alignas(64) PaddedIndex {
 	std::atomic<std::size_t> value{ 0 };
 };
@@ -11,7 +10,6 @@ struct alignas(64) PaddedIndex {
 template<typename T, size_t N>
 class RingQueue {
 public:
-
 	constexpr auto pop(T& item) -> bool {
 		const auto t = m_tail.value.load(std::memory_order_relaxed);
 		const auto h = m_head.value.load(std::memory_order_acquire);
@@ -22,7 +20,6 @@ public:
 
 		item = m_queue[t & (N - 1)];
 		m_tail.value.store(t + 1, std::memory_order_release);
-
 		return true;
 	}
 
@@ -33,8 +30,8 @@ public:
 		if ((h - t) == N)
 			return false;
 
-		m_queue[h & (N - 1)] = item;
-		m_head.value.store(h + 1, std::memory_order_release);
+			m_queue[h & (N - 1)] = item;
+			m_head.value.store(h + 1, std::memory_order_release);
 		return true;
 	}
 
@@ -47,19 +44,19 @@ private:
 
 int main() {
 	RingQueue<int, 8> q;
-
+	
 	std::thread prod([&] {
-		for (int i = 1; i <= 20; ++i)
-			while (!q.push(i));           // busy‑wait if full
-		});
+		for (int i { 1} ; i <= 20; ++i)
+			while (!q.push(i));   // busy‑wait if full
+	});
 
 	std::thread cons([&] {
 		int v;
-		for (int i = 1; i <= 20; ++i) {
+		for (int i { 1 }; i <= 20; ++i) {
 			while (!q.pop(v));
 			std::cout << v << ' ';
 		}
-		});
+	});
 
 	prod.join();  
 	cons.join();
